43edd635fb1a3e6015efb9276f3a3418568f143d
Eric Cheyne <eric.cheyne@gmail.com>
2024-06-11 03:43:27 -0700

    feat: add personal greeting

--- src/SimpleBot.java
+++ src/SimpleBot.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class SimpleBot {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2023.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String yourName = scanner.next();
+        System.out.println("What a great name you have, " + yourName + "!");
+
+        scanner.close();
     }
 }
